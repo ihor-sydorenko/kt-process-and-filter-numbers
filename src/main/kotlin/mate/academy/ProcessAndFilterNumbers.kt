3f008212ea375c8ef0457475c63dea3456b338ad
i.sydorenko <ihor@Administrators-Mac-mini.local>
2025-12-05 11:05:18 +0200

    valid sol

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    return numbers.asSequence()
+        .map { if (it % 2 == 0) it / 2 else it * 2 }
+        .filter { it > 25 }
+        .toList()
 }
