fd5e29d0a8c0d9e7186fd593f2e6bc44e3ccf642
i.sydorenko <ihor@Administrators-Mac-mini.local>
2025-12-05 11:34:09 +0200

    valid sol v5

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,8 +1,10 @@
 package mate.academy
 
+const val THRESHOLD = 25
+
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
     return numbers.asSequence()
         .map { if (it % 2 == 0) it / 2 else it * 2 }
-        .filter { it > 25 }
+        .filter { it > THRESHOLD }
         .toList()
 }
